b3c075cfee317814afdbb04bdf4fd05d06e8aa7e
John Franks <jrfranks@sveltesoft.com>
2023-06-24 18:31:27 -0700

    Simplify by using builtin voltage comparator.

--- MoistureController.ino
+++ MoistureController.ino
@@ -25,35 +25,84 @@
  * of soil and opens a valve when the soil is too dry.
  *
  * The threshold for the moisture level is controlled by a potentiometer,
- * which is an interrupt pin.
+ * which is attached to the internal comparator along with the moisture sensor.
  *
  * The program  puts the board into low-power sleep mode in the loop function
  * to minimize power consumption. When the moisture level falls below the
- * threshold, the valve is opened to water the soil. The valve is closed again
- * when the moisture level exceeds the threshold by 1%.
+ * threshold set by the POT, the valve is opened to water the soil. The valve 
+ * is closed again when the moisture level falls below the moisture sensor
+ * value.
  *
- * The program uses the LowPower library for low-power sleep mode.
-*/
+ * The program uses the LowPower library for low-power sleep mode and
+ * analogComp to manage the Arduino's comparator.
+ * 
+ * Note: The water valve that is attached must be a 'normally closed' valve.
+ * I.E. With no power to the valve the valve is closed.
+ */
 
 #include <LowPower.h>
+#include <analogComp.h>
 
-const int moistureSensorPin = A0; // Pin connected to moisture sensor
+const int moistureSensorPin = 6;  // Pin AIN0 connected to moisture sensor
+const int potPin = 7;             // Pin AIN1 connected to POT (interrupt pin)
 const int valvePin = 13;          // Pin connected to valve
-const int potPin = 2;             // Pin connected to potentiometer (interrupt pin)
-
-// Threshold values for moisture level (volatile for interrupt access)
-volatile int lowWater = 0;        // Low water level
-volatile int highWater = 1;       // high water level
-float        levelDelta = 1.01;   // 1% diff between low and high water levels
 
 // Function prototypes
 void setup(void);                 // Arduino initialization function
 void loop(void);                  // Arduino main loop
-void checkMoisture(void);         // Main moisture control function
-void setMoistureThreshold(void);  // Get POT reading and store it for use by checkMoisture()
-void handlePotInterrupt(void);    // Change POT reading when knob is moved.
-void openValve(void);             // Open water valve
-void closeValve(void);            // Close water valve
+void moistureLowInterrupt(void);
+void moistureHighInterrupt(void);
+
+#ifdef DEBUG
+volatile bool valveOpen = false;
+#endif /* DEBUG */
+
+
+/**
+ * @brief Closes the water valve.
+ *
+ * This function closes the water valve by setting the valvePin to LOW.
+ *
+ * This function is declared as static inline, which suggests to the compiler
+ * to insert the function's code directly into the place where it is called,
+ * instead of performing a regular function call.
+ *
+ * @note This function does not check if the valve is already closed. Calling
+ * this function when the valve is already closed will have no effect.
+ *
+ * @return void
+ */
+static inline void closeWaterValve(void)
+{
+  digitalWrite(valvePin, LOW); // Close valve.
+#ifdef DEBUG
+  valveOpen = false;
+#endif /* DEBUG */
+}
+
+
+/**
+ * @brief Opens the water valve.
+ *
+ * This function opens the water valve by setting the valvePin to HIGH.
+ *
+ * This function is declared as static inline to allow the compiler
+ * to insert the function's code directly into the place where it is called,
+ * instead of performing a regular function call.
+ *
+ * @note This function does not check if the valve is already opened. Calling
+ * this function when the valve is already opened will have no effect.
+ *
+ * @return void
+ */
+static inline void openWaterValve(void)
+{
+  digitalWrite(valvePin, HIGH); // Open valve.
+#ifdef DEBUG
+  valveOpen = true;
+#endif /* DEBUG */
+}
+
 
 /**
  * Sets up the initial state of the Arduino board and its peripherals.
@@ -63,15 +112,13 @@ void closeValve(void);            // Close water valve
  *
  * This function initializes the various pins and peripherals used by the
  * program, including the moisture sensor pin, the valve pin, and the POT pin.
- * It also sets up the interrupt service routines for the POT pin and the
- * moisture sensor pin.
- *
- * Additionally, the function sets the initial value for the moisture threshold
- * using the `setMoistureThreshold()` function, and initializes the Serial
- * communication for debugging purposes.
+ * It also sets up the interrupt service routines for the analog comparator 
+ * which measures the difference voltage differenct between the pot and
+ * the moisture sensor output pins.
  *
  * Finally, the function enters a low-power mode by calling
- * `LowPower.powerDown()` to conserve power until the next interrupt occurs.
+ * LowPower.powerDown() to conserve power until the next comparator 
+ * interrupt occurs.
  */
 
 void setup(void)
@@ -82,7 +129,7 @@ void setup(void)
 
   // Wait for Serial connection to be established
   while (!Serial) {
-    ; // Do nothing
+    // Do nothing
   }
 
   // Print debug message to Serial monitor
@@ -92,24 +139,13 @@ void setup(void)
   // Disable interrupts until all the subsystems are initialized.
   noInterrupts();
 
-  // Initialize POT
-  pinMode(potPin, INPUT);
-  attachInterrupt(digitalPinToInterrupt(potPin), setMoistureThreshold, CHANGE); // Set interrupt on potentiometer pin
-
   // Initialize valve
   pinMode(valvePin, OUTPUT);
-  digitalWrite(valvePin, LOW); // Make sure valve is closed.
+  digitalWrite(valvePin, LOW);     // Make sure valve is closed.
 
-  // Initialize moisture sensor
-  pinMode(moistureSensorPin, INPUT);
-  attachInterrupt(digitalPinToInterrupt(moistureSensorPin), checkMoisture, CHANGE); // Set interrupt on moisture sensor pin
-
-  // Set initial valve state
-  setMoistureThreshold();
-
-  // Open / close valve appropriately
-  checkMoisture();
-
+  analogComparator.setOn(AIN0, AIN1);    // Use voltages on both pins to compare
+  analogComparator.enableInterrupt(moistureLowInterrupt, FALLING);
+  
   // System is in order so we can enable interrupts and start normal processing.
   interrupts();
 }
@@ -135,74 +171,25 @@ void setup(void)
 void loop(void)
 {
   // Put board into sleep mode
-  LowPower.powerDown(SLEEP_MODE_IDLE, ADC_OFF, BOD_OFF);
-}
-
-
-/**
- * Sets the moisture threshold for activating the valve.
- *
- * @param threshold An integer value representing the moisture level threshold
- *                  as a percentage, ranging from 0 (completely dry) to 100
- *                  (completely saturated). This value is controlled by a
- *                  potentiometer connected to the Arduino, and should be
- *                  in the range of 0-1023.
- * @return None.
- *
- * This function sets the moisture threshold for activating the valve when the
- * soil is too dry. The threshold is a percentage value that is controlled by
- * a potentiometer connected to the Arduino. The function reads the current
- * value of the potentiometer and calculates the corresponding threshold value,
- * which is then stored in a global variable for use in the `checkMoisture()`
- * function. If the potentiometer value is below a certain minimum threshold,
- * the function will set the threshold to the minimum value to prevent the
- * valve from being activated unnecessarily.
- */
-
-void setMoistureThreshold(void) {
-  int potValue = analogRead(potPin);
-
-  // Normalize input level
-  lowWater = map(potValue, 0, 1023, 0, 1023);
-
-  // Calculate highWater here to avoid constant math during level interrupts.
-  highWater = (int)(lowWater * levelDelta);
-  if (highWater <= lowWater) {
-    highWater = lowWater + 1;
+  LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
+#ifdef DEBUG
+  if (valveOpen == true) {
+    Serial.println("Valve OPEN");
+  } else {
+    Serial.println("Valve CLOSED");
   }
+#endif /* DEBUG */
 }
 
 
-/**
- * Checks the moisture level in the soil and activates the valve if necessary.
- *
- * @param None.
- *
- * @return None.
- *
- * This function checks the moisture level of the soil and opens the valve
- * if the moisture level is below the threshold. The threshold is set by
- * the value of the `threshold` variable, which is updated whenever the
- * potentiometer changes. The `threshold` variable should be a value between
- * 0 and 1023, corresponding to the range of values output by the potentiometer.
- * If the moisture level is below the threshold, the `valveOpen` variable is
- * set to `true` and the valve is opened. If the moisture level is above the
- * threshold by 5% or more, the `valveOpen` variable is set to `false` and
- * the valve is closed.
- */
-
-void checkMoisture(void)
+void moistureLowInterrupt(void)
 {
-  static bool valveOpen = false; // Flag to keep track of valve status
+  openWaterValve();
+  analogComparator.enableInterrupt(moistureHighInterrupt, FALLING);
+}
 
-  int moistureLevel = analogRead(moistureSensorPin);
-
-  moistureLevel = map(moistureLevel, 0, 1023, 0, 1023);
-  if (!valveOpen && (moistureLevel < lowWater)) {
-    digitalWrite(valvePin, HIGH); // Open valve
-    valveOpen = true;
-  } else if (valveOpen && (moistureLevel >= highWater)) {
-    digitalWrite(valvePin, LOW); // Close valve
-    valveOpen = false;
-  }
+void moistureHighInterrupt(void)
+{
+  closeWaterValve();
+  analogComparator.enableInterrupt(moistureLowInterrupt, RISING);
 }
